957e0d2a109e025e898dfc73883d328e2b89cc97
Krunal Panchal <kgp.opensource@gmail.com>
2016-05-30 02:16:17 +0530

    Delete Start.java

--- Start.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class Start 
-{
-	public static void main(String[] args) throws IOException
-	{
-		Process process = Runtime.getRuntime().exec("java -Xms150m -Xmx400m -jar JRC.jar",null);
-	}
-}
